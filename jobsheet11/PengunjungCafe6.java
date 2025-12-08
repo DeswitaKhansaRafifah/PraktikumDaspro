5f6209edb309941b4aea5e25a219719c588fea66
DeswitaKhansaRafifah <deswitakhansa924@gmail.com>
2025-12-08 22:37:21 +0700

    Percobaan 4

---
+++ jobsheet11/PengunjungCafe6.java
@@ -0,0 +1,19 @@
+package jobsheet11;
+
+import java.util.Scanner;
+public class PengunjungCafe6 {
+    static void daftarPengunjung(String... namaPengunjung) {
+        System.out.println("Daftar Nama pengunjung:");
+        for(int i = 0; i < namaPengunjung.length; i++) {
+            System.out.println("- "+namaPengunjung[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        daftarPengunjung("Ali", "Budi", "Citra");
+
+        daftarPengunjung("Andi");
+        daftarPengunjung("Doni", "Eti", "Fahmi", "Galih");
+    }
+    
+}
